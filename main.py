2af6cef5aa76026396bff8b5b8e42fea8f601d27
SebekBr <sebastian.brodziak@gmail.com>
2020-10-05 20:54:11 +0200

    Some changes and ready result?

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ A = np.array([[1,2,-1,3,-4,-1,-2,2],
              [3,1,4,-5,-5,1,3,-4],
              [2,-2,2,2,4,-9,-2,4],
              [4,5,8,-1,-6,2,2,-5],
-             [-2,2,1,6,1,-4,-5,2]])
+             [-2,2,1,6,1,-4,-5,2]], dtype = np.float)
 
-B = np.array([10,2,-5,3,6,-2,-4,9])
+B = np.array([10,2,-5,3,6,-2,-4,9], dtype = np.float)
 
-print('matrix A\n', A)
-
-print(np.linalg.det(A))
+#print('matrix A\n', A)
+#print(np.linalg.det(A))
+x = np.linalg.solve(A,B)
+print(x)
