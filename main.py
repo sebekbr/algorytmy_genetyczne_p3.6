7d9926f8e263a04daa3d456e321f4d68a8e81574
SebekBr <sebastian.brodziak@gmail.com>
2020-10-05 19:14:27 +0200

    new B matrix

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ A = np.array([[1,2,-1,3,-4,-1,-2,2],
              [4,5,8,-1,-6,2,2,-5],
              [-2,2,1,6,1,-4,-5,2]])
 
+B = np.array([10,2,-5,3,6,-2,-4,9])
+
 print('matrix A\n', A)
 
 print(np.linalg.det(A))
 
+rownanie = 1*a + 2*b + -1*c
