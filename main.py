4d609b56cf1284182b8d48cf767022a7d803fb68
SebekBr <sebastian.brodziak@gmail.com>
2020-10-09 21:38:59 +0200

    Code reorganisation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Sebastian Brodziak - algorytmy genetyczne v0.1
 import numpy as np
+import random
 A = np.array([[1,2,-1,3,-4,-1,-2,2],
              [-3,-1,2,-3,4,-5,-2,-1],
              [1,-3,9,2,-8,3,-2,7],
@@ -21,5 +22,14 @@ B = np.array([10,2,-5,3,6,-2,-4,9], dtype = np.float)
 #print(lista[:4])
 #print(lista[4:])
 
-randnums = np.random.randint(-10,10,8)
-print(randnums)
+# randnums = np.random.randint(-10,10,8)
+# print(randnums)
+
+# random_numbers = random.seed(100)
+# population=[[random.randint(-10,10) for i in range(8)] for j in range(8)] #i - generator wiersza, j - generator kolumn
+# print(population)
+
+population=[[1,2,-1,3,-4,-1,-2,2],[-3,-1,2,-3,4,-5,-2,-1],[1,-3,9,2,-8,3,-2,7],[2,-1,6,2,3,6,-9,-1],
+             [3,1,4,-5,-5,1,3,-4],[2,-2,2,2,4,-9,-2,4],[4,5,8,-1,-6,2,2,-5],[-2,2,1,6,1,-4,-5,2]]
+
+fitness_score = sum([10,2,-5,3,6,-2,-4,9])
