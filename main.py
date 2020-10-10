5aaf11ae7eb076a1e98aa0b50cbb3fbba4f896a8
SebekBr <sebastian.brodziak@gmail.com>
2020-10-10 11:31:59 +0200

    creating temp file

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
 # Sebastian Brodziak - algorytmy genetyczne v0.2
 import random
 
-# równanie
-# a, b, c, d, e, f, g, h, x0, x1, x2, x3, x4, x5, x6, x7 = 0
-# eq = a*x0 + b*x1 + c*x2 + d*x3 + e*x4 + f*x5 + g*x6 + h*x7
-
-# generowanie liczb losowych
-# a = []
-# x = []
-#
-# for i in range(8):
-#     a.append(random.randint(-10, 10))
-#     x.append(random.randint(-10, 10))
-# print(a)
-# print(x)
-
-# Mnożenie a*x
-# multi = []
-# for i in range(8):
-#     multi.append(a[i]*x[i])
-#
-# print(multi)
-
 # populacja
 population = [[1, 2, -1, 3, -4, -1, -2, 2],
               [-3, -1, 2, -3, 4, -5, -2, -1],
---
+++ temp.py
@@ -0,0 +1,20 @@
+# równanie
+# a, b, c, d, e, f, g, h, x0, x1, x2, x3, x4, x5, x6, x7 = 0
+# eq = a*x0 + b*x1 + c*x2 + d*x3 + e*x4 + f*x5 + g*x6 + h*x7
+
+# generowanie liczb losowych
+# a = []
+# x = []
+#
+# for i in range(8):
+#     a.append(random.randint(-10, 10))
+#     x.append(random.randint(-10, 10))
+# print(a)
+# print(x)
+
+# Mnożenie a*x
+# multi = []
+# for i in range(8):
+#     multi.append(a[i]*x[i])
+#
+# print(multi)
