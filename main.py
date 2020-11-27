8559b61128f1eef52b052592333d97bdb8d77091
SebekBr <sebastian.brodziak@gmail.com>
2020-11-27 21:54:05 +0100

    cleaning

--- main.py
+++ main.py
@@ -11,43 +11,10 @@
 import numpy as np
 import random as rand
 
-# populacja
-constant_population = np.array([
-    [1, 2, -1, 3, -4, -1, -2, 2],
-    [-3, -1, 2, -3, 4, -5, -2, -1],
-    [1, -3, 9, 2, -8, 3, -2, 7],
-    [2, -1, 6, 2, 3, 6, -9, -1],
-    [3, 1, 4, -5, -5, 1, 3, -4],
-    [2, -2, 2, 2, 4, -9, -2, 4],
-    [4, 5, 8, -1, -6, 2, 2, -5],
-    [-2, 2, 1, 6, 1, -4, -5, 2]])
-
 # Populacja z wartościami losowymi
 population = np.random.randint(-10, 10, size=(8, 8))
 
 br = np.array([10, 2, -5, 3, 6, -2, -4, 9])
-
-# Stała macierz
-constant_matrix = np.array([[2, -5, 3, 2, 5, 9, -10, 1],
-                   [-6, 4, -4, -4, -1, 10, 1, -2],
-                   [-5, -6, -8, -9, 2, 2, 7, 10],
-                   [7, -5, -2, -4, -4, 7, 1, -1],
-                   [8, -10, 1, -9, 5, -7, 0, 9],
-                   [-3, -9, 5, 10, 2, 2, -9, -5],
-                   [-8, -3, 10, 2, 3, 7, -9, 1],
-                   [9, 7, -9, 1, 5, -6, -4, 5],
-                   [3, 10, 1, 4, 9, 5, -6, 1],
-                   [-9, 6, 8, 4, -10, 4, -3, 4],
-                   [7, -3, -5, 3, 9, 8, 0, 3],
-                   [-3, 0, 10, -2, -1, -8, 9, 8],
-                   [4, -1, 2, 0, -6, 6, -6, 3],
-                   [9, 3, 6, 6, 4, -4, 10, -1],
-                   [8, 8, 10, -3, -4, 0, -10, -5],
-                   [-8, 0, 10, 5, 2, 1, -3, -7],
-                   [5, -4, 9, -6, 0, -5, -9, -9],
-                   [6, -6, 0, 1, -1, 5, -9, 7],
-                   [-10, -2, -6, -9, 8, -9, 5, -6],
-                   [0, -5, 0, 2, 5, 9, -10, 1]])
 
 # Macierz z wartościami losowymi
 matrix = np.random.randint(-10, 10, size=(20, 8))
@@ -70,12 +37,11 @@ def wsp_dost():
 
 sorted_matrix = np.array([y for x, y in sorted(zip(wsp_dost(), matrix))])
 
-
-# punkt krzyżowania
-cross_point = int(len(matrix[0]) / 2)
-
 # KRZYŻOWANIE
 def cross():
+    # punkt krzyżowania
+    cross_point = int(len(matrix[0]) / 2)
+
     crossed = []
     for i in range(0, len(sorted_matrix)-1, 2):
         crossed.append(np.append(sorted_matrix[i][:cross_point], sorted_matrix[i+1][cross_point:]))
@@ -87,7 +53,8 @@ crossed_array = cross()
 
 print(cross())
 
-# Mutation
+
+# MUTACJA
 def mutation(loop_count):
     for i in range(loop_count):
         random_index = rand.randint(0, len(crossed_array[0])-1)
@@ -100,10 +67,4 @@ def mutation(loop_count):
     return crossed_array
 
 
-# print(matrix)
-# print("--------------")
-# print(sorted_matrix)
-# print("--------------")
-# print(cross())
-# print("--------------")
 print(mutation(4))
